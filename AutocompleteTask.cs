e0662cf446b9d1d7dd276f8b31c34d6207823f04
NaNlaggergit <hlo.lyashenko@yandex.ru>
2024-10-20 16:44:28 +0700

    Add project files.

---
+++ AutocompleteTask.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using NUnit.Framework;
+
+namespace Autocomplete;
+
+internal class AutocompleteTask
+{
+	/// <returns>
+	/// Возвращает первую фразу словаря, начинающуюся с prefix.
+	/// </returns>
+	/// <remarks>
+	/// Эта функция уже реализована, она заработает, 
+	/// как только вы выполните задачу в файле LeftBorderTask
+	/// </remarks>
+	public static string FindFirstByPrefix(IReadOnlyList<string> phrases, string prefix)
+	{
+		var index = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1;
+		if (index < phrases.Count && phrases[index].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
+			return phrases[index];
+            
+		return null;
+	}
+
+	/// <returns>
+	/// Возвращает первые в лексикографическом порядке count (или меньше, если их меньше count) 
+	/// элементов словаря, начинающихся с prefix.
+	/// </returns>
+	/// <remarks>Эта функция должна работать за O(log(n) + count)</remarks>
+	public static string[] GetTopByPrefix(IReadOnlyList<string> phrases, string prefix, int count)
+	{
+		var phrasesCount = phrases.Count;
+		var leftBorder=LeftBorderTask.GetLeftBorderIndex(phrases,prefix,-1, phrasesCount)+1;
+		if (leftBorder == phrasesCount)
+			return new string[0];
+		var actualCount =Math.Min(count, phrasesCount-leftBorder);
+		var result=new List<string>();
+		var nextPhraseIndex = 0;
+		for(int i = 0; i < actualCount; i++)
+		{
+			nextPhraseIndex = leftBorder + i;
+			if (!phrases[nextPhraseIndex].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+				break;
+			result.Add(phrases[nextPhraseIndex]);
+		}
+		// тут стоит использовать написанный ранее класс LeftBorderTask
+		return result.ToArray();
+	}
+
+	/// <returns>
+	/// Возвращает количество фраз, начинающихся с заданного префикса
+	/// </returns>
+	public static int GetCountByPrefix(IReadOnlyList<string> phrases, string prefix)
+	{
+		var left = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count);
+		var right=RightBorderTask.GetRightBorderIndex(phrases,prefix,-1,phrases.Count);
+		var result = right - left - 1;
+		// тут стоит использовать написанные ранее классы LeftBorderTask и RightBorderTask
+		return result;
+	}
+}
+
+[TestFixture]
+public class AutocompleteTests
+{
+	[Test]
+	public void TopByPrefix_IsEmpty_WhenNoPhrases()
+	{
+		var phrases =new List<string>();
+		var result = AutocompleteTask.GetTopByPrefix(phrases, "q",0);
+		CollectionAssert.IsEmpty(result);
+	}
+
+	// ...
+
+	[Test]
+	public void CountByPrefix_IsTotalCount_WhenEmptyPrefix()
+	{
+		// ...
+		//Assert.AreEqual(expectedCount, actualCount);
+	}
+
+	// ...
+}
---
+++ RightBorderTask.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+
+namespace Autocomplete;
+
+public class RightBorderTask
+{
+    /// <returns>
+    /// Возвращает индекс правой границы. 
+    /// То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
+    /// Если такого нет, то возвращает items.Length
+    /// </returns>
+    /// <remarks>
+    /// Функция должна быть НЕ рекурсивной
+    /// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
+    /// </remarks>
+    public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+    {
+        int mid = (right - left) / 2 + left;
+        if(phrases.Count==0)
+            return -1;
+        while (right - left > 1)
+        {
+            mid = (left + right) / 2;
+            if (string.Compare(prefix, phrases[mid], StringComparison.InvariantCultureIgnoreCase) >= 0
+                || phrases[mid].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                left = mid;
+            else
+                right = mid;
+        }
+        return right;
+        // IReadOnlyList похож на List, но у него нет методов модификации списка.
+        // Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
+        /*		for (int i = phrases.Count-1; i >= 0; i--)
+                {
+                    if (string.Compare(prefix, phrases[i], StringComparison.InvariantCultureIgnoreCase) >= 0 
+                        || phrases[i].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
+                        return i + 1;
+                }
+                return 0;*/
+    }
+}
